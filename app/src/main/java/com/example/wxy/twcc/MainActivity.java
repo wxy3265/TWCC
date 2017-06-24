f45367b3b7086ba5a3a80daa24f38b3d4aeab523
wxy3265 <546668033@qq.com>
2017-06-24 22:06:19 +0800

    First commit

---
+++ app/src/main/java/com/example/wxy/twcc/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.wxy.twcc;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.et_input);
+        //editText.addTextChangedListener();
+
+    }
+}
